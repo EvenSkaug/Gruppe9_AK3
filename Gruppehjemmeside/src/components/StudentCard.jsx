da2cae71b0e7b1d5591df368c3bbae5bc0f136a7
jave <111729472+mokkajave@users.noreply.github.com>
2025-03-28 16:18:05 +0100

    endret "biografi" til "epost" i profilkort

--- Gruppehjemmeside/src/components/StudentCard.jsx
+++ Gruppehjemmeside/src/components/StudentCard.jsx
@@ -1,11 +1,11 @@
 import "../styles/studentCard.scss";
 
-export default function StudentCard({name, image, biography}) {
+export default function StudentCard({name, image, epost}) {
     return (
         <article className="student-card">
             <img src={image} alt="student-image" />
             <h3>{name}</h3>
-            <p>{biography}</p>
+            <p>{epost}</p>
         </article>
     )
 }
--- Gruppehjemmeside/src/components/StudentCards.jsx
+++ Gruppehjemmeside/src/components/StudentCards.jsx
@@ -4,9 +4,9 @@ import "../styles/studentCards.scss";
 
 export default function StudentCards() {
     const students = [
-        {id: 1, name: "Stian", image: "../200x300.png", biography: "Desrciption"},
-        {id: 2, name: "Even", image: "../200x300.png", biography: "Description"},
-        {id: 3, name: "Simon", image: "../200x300.png", biography: "Description"}
+        {id: 1, name: "Stian", image: "../200x300.png", epost: "stian@email.com"},
+        {id: 2, name: "Even", image: "../200x300.png", epost: "even@email.com"},
+        {id: 3, name: "Simon", image: "../200x300.png", epost: "simon@email.com"}
       ]
 
     return (
@@ -17,7 +17,7 @@ export default function StudentCards() {
                     key={student.id} // Unique student based on their id
                     image={student.image}
                     name={student.name}
-                    biography={student.biography}
+                    epost={student.epost}
                 />
             ))}
         </div>
