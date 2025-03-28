b2b2137872048e2b45ae79e301fbffd5242cb3e4
jave <111729472+mokkajave@users.noreply.github.com>
2025-03-28 17:49:32 +0100

    la til hero-section
    banner på hjemmesiden

--- Gruppehjemmeside/src/components/Home.jsx
+++ Gruppehjemmeside/src/components/Home.jsx
@@ -5,6 +5,11 @@ import "../styles/home.scss";
 export default function Home() {
     return (
         <>
+        <section className="hero-section grid">
+            <article>
+                <h1>Arbeidskrav 3</h1>
+            </article>
+        </section>
         <section className="student-cards-section grid">
             <h2 className="student-cards-section-title">Gruppe 9</h2>
 
