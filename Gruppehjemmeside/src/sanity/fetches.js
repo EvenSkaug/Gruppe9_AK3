f436438e7380f2e6e5c8e56a31b814262ee38182
jave <111729472+mokkajave@users.noreply.github.com>
2025-04-19 14:19:35 +0200

    flere kommentarer

--- Gruppehjemmeside/src/sanity/fetches.js
+++ Gruppehjemmeside/src/sanity/fetches.js
@@ -1,30 +1,31 @@
 import { client } from "./client";
 
-// Fetcher ut alle medlemmer, og nødvendige verdier
-export async function fetchAllMembers() {
+  // Fetcher alle medlemmer, og nødvendige verdier
+  export async function fetchAllMembers() {
     const data = await client.fetch(
       `*[_type == "members"]{_id, name, navname, image{asset ->{ _id, url }}, email, description, "slug" : slug.current, worklog[] { date, description, _key }}`
     );
     return data;
   }
 
-// Fetcher ut medlemmer på slug
-export async function fetchMemberBySlug(slug) {
+  // Fetcher medlem på slug, og nødvendige verdier
+  export async function fetchMemberBySlug(slug) {
     const data = await client.fetch(
-        `*[_type == "members" && slug.current == "${slug}"]{_id, name, image { asset -> { _id, url } }, email, interests, description, "slug" : slug.current, worklog[] { date, description, _key }
-        }`
+      `*[_type == "members" && slug.current == "${slug}"]{_id, name, image { asset -> { _id, url } }, email, interests, description, "slug" : slug.current, worklog[] { date, description, _key }}`
     );
     return data;
   }
 
-export async function fetchAllWorkLogs() {
+  // Fetcher alle arbeidslogger
+  export async function fetchAllWorkLogs() {
     const data = await client.fetch(
       `*[_type == "members"].worklog[]{date, description, _key} | order(date desc)`
     );
     return data;
   }
 
-export async function fetchWorkLogsByMember(memberSlug) {
+  // Fetcher arbeidslogg per medlem på slug
+  export async function fetchWorkLogsByMember(memberSlug) {
     const data = await client.fetch(
       `*[_type == "members" && slug.current == "${memberSlug}"]{worklog[]{date, description, _key}}[0].worklog | order(date desc)`
     );
